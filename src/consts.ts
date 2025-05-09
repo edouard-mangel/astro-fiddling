6aa9d7aa756ed7d9731966c8c3dea07c6f47f069
Edouard <mangel.edouard.pro@gmail.com>
2025-05-09 12:32:41 +0200

    feat: transfo to La Crafterie Tech

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Vitrinéo';
-export const SITE_DESCRIPTION = 'La vitrine en ligne des PME près de chez vous !';
+export const SITE_TITLE = 'La Crafterie Tech';
+export const SITE_DESCRIPTION = 'Vos applications spécifiques sur mesure, avec un focus sur la qualité !';
